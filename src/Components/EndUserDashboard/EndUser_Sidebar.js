e22c3fb16ac6e091251268e2c6c4504c4a86f734
Ayesha <ayeshalatif9778@gmail.com>
2025-04-25 10:07:57 +0500

    first commit

---
+++ src/Components/EndUserDashboard/EndUser_Sidebar.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom'; // Import Link for routing
+import {  FaUserFriends } from 'react-icons/fa';
+import logo from "../assets/images/uet-pic.png"
+import './EndUser_Sidebar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPerson } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
+function EndUserSidebar() {
+  const [counts, setCounts] = useState({
+    students: 0,
+    totalComplaints: 0,
+    resolvedComplaints: 0,
+    pendingComplaints: 0,
+  });
+
+  useEffect(() => {
+    const fetchCounts = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:5000/api/users/stats"
+        );
+        setCounts(response.data);
+      } catch (error) {
+        console.error("Error fetching counts:", error.message);
+      }
+    };
+
+    fetchCounts();
+  }, []);
+
+  return (
+    <div className="Endsidebar">
+      <img src={logo} className='EndUETLOGO'/>
+      <h3>EndUser's Dashboard</h3>
+      {/* <div className="menu-item">
+        <FaClipboardList style={{ marginRight: '8px' }} />
+        <span>Complaint</span>
+      </div> */}
+
+      <div className="Endmenu-item">
+        {/* <span>End Users</span> */}
+
+        <ul>
+        <li className="endusers-listss">
+          <Link to="/endusers-listss">
+            <span>Complaints</span><FontAwesomeIcon className="EndUser-Icon" icon={faPerson} />
+          </Link>
+        </li>
+        </ul>
+
+      </div>
+      <div className="Endstats">
+        <div className="Endstat-box">
+          <p>Total complaints</p>
+          <h4>{counts.totalComplaints}</h4>
+        </div>
+        <div className="Endstat-box">
+          <p>Resolved</p>
+          <h4>{counts.resolvedComplaints}</h4>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default EndUserSidebar
